4fa00f3fa7563bd4495e90153e75f39e358337d6
Nisanth Chunduru <nisanth074@gmail.com>
2023-11-27 16:24:41 +0530

    Add balance sheet and loan approval pages

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/loan_applications/new')
+def new_loan_application():
+    return render_template('loan_applications/new.html')
+
+@app.route('/loan_applications/balance_sheet')
+def balance_sheet():
+    return render_template('loan_applications/balance_sheet/show.html')
+
+@app.route('/loan_applications/approved')
+def approved():
+    return render_template('loan_applications/approved/show.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
